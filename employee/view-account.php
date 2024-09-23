00e39f41e211b16195c0f006a1f8fb6779e0ae63
Thenmozhi <cthenmozhi96@gmail.com>
2024-09-23 21:08:15 +0530

    emp view-bank pg design change it

--- employee/view-account.php
+++ employee/view-account.php
@@ -32,7 +32,7 @@ $passbookImgSrc = !empty($account['passbook_img']) ? 'data:image/jpeg;base64,' .
 <html lang="en">
 <head>
     <?php include('vendor/inc/head.php'); ?>
-    <link rel="stylesheet" href="vendor/css/account.css?v=1.0">
+    <link rel="stylesheet" href="vendor/css/account.css">
     <title>View Account</title>
 </head>
 <body>
@@ -43,9 +43,9 @@ $passbookImgSrc = !empty($account['passbook_img']) ? 'data:image/jpeg;base64,' .
             <div class="card card-1">
                 <div class="card-body">
                     <h2 class="h2">Account Details</h2>
-                    <div class="account-details">
-                         <!-- Passbook Image -->
-                         <div class="input-group">
+                    <div class="account-details-container">
+                        <!-- Passbook Image -->
+                        <div class="passbook-img-container">
                             <p>Passbook Image</p>
                             <?php if (!empty($passbookImgSrc)): ?>
                                 <img class="passbook-img" src="<?php echo $passbookImgSrc; ?>" alt="Passbook Image">
@@ -53,46 +53,49 @@ $passbookImgSrc = !empty($account['passbook_img']) ? 'data:image/jpeg;base64,' .
                                 <p>No Image Available</p>
                             <?php endif; ?>
                         </div>
-                        <!-- First Row: Account Holder Name, Bank Name -->
-                        <div class="row">
-                            <div class="col">
-                                <div class="input-group">
-                                    <p>Account Holder Name</p>
-                                    <input class="input--style-1" type="text" value="<?php echo $accountHolder; ?>" readonly>
+
+                        <!-- Account Details -->
+                        <div class="account-details">
+                            <!-- First Row: Account Holder Name, Bank Name -->
+                            <div class="row">
+                                <div class="col">
+                                    <div class="input-group">
+                                        <p>Account Holder Name</p>
+                                        <input class="input--style-1" type="text" value="<?php echo $accountHolder; ?>" readonly>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="input-group">
+                                        <p>Bank Name</p>
+                                        <input class="input--style-1" type="text" value="<?php echo $bankName; ?>" readonly>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col">
-                                <div class="input-group">
-                                    <p>Bank Name</p>
-                                    <input class="input--style-1" type="text" value="<?php echo $bankName; ?>" readonly>
+
+                            <!-- Second Row: Account Number -->
+                            <div class="input-group">
+                                <p>Account Number</p>
+                                <input class="input--style-1" type="text" value="<?php echo $accountNumber; ?>" readonly>
+                            </div>
+
+                            <!-- Third Row: IFSC Code, Branch Name -->
+                            <div class="row">
+                                <div class="col">
+                                    <div class="input-group">
+                                        <p>IFSC Code</p>
+                                        <input class="input--style-1" type="text" value="<?php echo $ifscCode; ?>" readonly>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="input-group">
+                                        <p>Branch Name</p>
+                                        <input class="input--style-1" type="text" value="<?php echo $branchName; ?>" readonly>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
-                        <!-- Second Row: Account Number -->
-                        <div class="input-group">
-                            <p>Account Number</p>
-                            <input class="input--style-1" type="text" value="<?php echo $accountNumber; ?>" readonly>
-                        </div>
-
-                        <!-- Third Row: IFSC Code, Branch Name -->
-                        <div class="row">
-                            <div class="col">
-                                <div class="input-group">
-                                    <p>IFSC Code</p>
-                                    <input class="input--style-1" type="text" value="<?php echo $ifscCode; ?>" readonly>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="input-group">
-                                    <p>Branch Name</p>
-                                    <input class="input--style-1" type="text" value="<?php echo $branchName; ?>" readonly>
-                                </div>
-                            </div>
-                        </div>
-
-                       
                     </div>
+
                     <button class="btn btn--radius btn--blue" onclick="window.location.href='my-profile.php?emp_id=<?php echo $id; ?>';">Back to Profile</button>
                 </div>
             </div>
